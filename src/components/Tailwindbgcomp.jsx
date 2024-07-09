48b1fd222d44722d43c3bc8bdfe70f42ddd5c858
Nitin111singh <nitinsingh80663@gmail.com>
2024-07-10 03:14:51 +0530

    added navbar changes

---
+++ src/components/Tailwindbgcomp.jsx
@@ -0,0 +1,7 @@
+export function TailwindBackground() {
+  return (
+    <div className="relative size-full bg-neutral-900">
+      <div className="absolute inset-0 bg-fuchsia-400 bg-[size:20px_20px] opacity-20 blur-[100px]" />
+    </div>
+  );
+}
